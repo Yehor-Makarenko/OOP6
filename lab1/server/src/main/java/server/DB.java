0f46ba3ab341e7b74298e73370ec100f8025e3da
Yehor Makarenko <mak.yegor@gmail.com>
2024-04-06 22:10:03 +0300

    Rename

---
+++ lab1/server/src/main/java/server/DB.java
@@ -0,0 +1,25 @@
+package server;
+
+import java.sql.*;
+import java.util.Properties;
+
+public class DB {
+  public static void main(String[] args) {
+    String url = "jdbc:postgresql://localhost:5432/test_db";
+    Properties props = new Properties();
+    props.setProperty("user", "postgres");
+    props.setProperty("password", "admin");
+    Connection conn = null;
+    try {
+      conn = DriverManager.getConnection(url, props);
+      Statement stmt = conn.createStatement();
+      ResultSet result = stmt.executeQuery("SELECT * FROM users WHERE id<3");
+      while (result.next()) {
+        String value = result.getString("name");
+        System.out.println(value);
+      }
+    } catch (SQLException e) {      
+      e.printStackTrace();
+    }
+  }
+}
---
+++ lab1/server/src/main/java/server/SSExample.java
@@ -0,0 +1,32 @@
+package server;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/events")
+public class SSExample extends HttpServlet {
+
+  @Override
+  protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    response.setContentType("text/event-stream");
+    response.setCharacterEncoding("UTF-8");
+
+    PrintWriter out = response.getWriter();    
+    for (int i = 0; i < 3; i++) {
+      out.print("retry: 0\ndata: Connection..\nid:" + i + "\n\n");
+      out.flush();
+
+      try {
+        Thread.sleep(1000); // Задержка между событиями
+      } catch (InterruptedException e) {
+        Thread.currentThread().interrupt();
+      }
+    }
+    out.close();
+  }
+}
